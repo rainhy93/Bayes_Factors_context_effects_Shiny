f4f88bfa052ed451404a4d3d152a07504d7f0e62
rainhy93 <yuh4@illinois.edu>
2022-10-25 15:52:45 -0500

    1. computed more BFs 2. modified the function "get_bf_individual" 3. modified the UI to allow more customizations

--- app.R
+++ app.R
@@ -7,10 +7,9 @@ library(readr)
 source("get_bf_individual.R")
 
 
-bfs_all_sub_00_to_33 <- read_csv("./bfs_all_sub_00_to_33.csv", 
-                                 col_types = cols(...1 = col_skip()))
+bfs_all_sub_00_to_90 <- read_csv("results_00_to_90.csv")
 
-subjects = bfs_all_sub_00_to_33 %>% select(Subject) %>% distinct(.) %>% pull(-1)
+subjects = bfs_all_sub_00_to_90 %>% select(Subject) %>% distinct(.) %>% pull(-1)
 
 
 
@@ -18,123 +17,203 @@ ui = fluidPage(
   titlePanel("Bayes Factors for Your Context Effects Specification"),
   
   sidebarLayout(
-    
     sidebarPanel(
-      p("Select a value for the width of indifference between A and B:"),
-      selectInput("tau", "Select tau",
-                  choices = c(seq(0,0.33,by=0.05),0.33)),
+      # let the user choose two tau values: one for preference, the other for indifference
       
-      p("Choose location bins of an alternative that if added to the the choices between A and B, will change a decision maker's preference:"),
-      splitLayout(
-        checkboxGroupInput("bins_pref_a", "Increase preference for A", 
-                         choiceNames = paste0("Bin ", 1:34),
-                         choiceValues = 1:34),
-        
-        checkboxGroupInput("bins_pref_b", "Increase preference for B", 
-                           choiceNames = paste0("Bin ", 1:34),
-                           choiceValues = 1:34)
-      )
+      selectInput(
+        "error_pref",
+        "Select the value of maximum deviation from a deterministic choice:",
+        choices = seq(0.05, 0.5, by = 0.025)
       ),
-    mainPanel(h3("Attribute Map", align = "left"),
-              img(src = "attribute map.png", height = 550, width = 700,  
-                  style = "display: block; margin-left: auto; margin-right: auto;"),
-              tags$br(),
-              actionButton(inputId = "go", label = "Run Analysis"),
-              plotlyOutput("bf_dist"),
-              tags$hr(),
-              textOutput("group_bf"),
-              # export bfs
-              downloadButton(outputId = "exportBFs", label = "Export Bayes Factors")
+      uiOutput("error_pref_selection"),
+      
+      
+      selectInput(
+        "error_indiff",
+        "Select the value of maximum deviation from coin flippng:",
+        choices = seq(0, 0.45, by = 0.025)
+      ),
+      uiOutput("error_indiff_selection"),
+      
+      
+      p("Select the predicted patterns for bins based on your hypothesis:"),
+      splitLayout(
+        checkboxGroupInput(
+          "bins_pref_a",
+          "A",
+          choiceNames = paste0("Bin ", 1:34),
+          choiceValues = 1:34
+        ),
+        checkboxGroupInput(
+          "bins_indiff",
+          "Indifferent",
+          choiceNames = paste0("Bin ", 1:34),
+          choiceValues = 1:34
+        ),
+        checkboxGroupInput(
+          "bins_pref_b",
+          "B",
+          choiceNames = paste0("Bin ", 1:34),
+          choiceValues = 1:34
+        )
+      )
     ),
+    mainPanel(
+      h3("Attribute Map", align = "left"),
+      img(
+        src = "attribute map.png",
+        height = 550,
+        width = 700,
+        style = "display: block; margin-left: auto; margin-right: auto;"
+      ),
+      tags$br(),
+      actionButton(inputId = "go", label = "Run Analysis"),
+      plotlyOutput("bf_dist"),
+      tags$hr(),
+      textOutput("group_bf"),
+      # export bfs
+      downloadButton(outputId = "exportBFs", label = "Export Bayes Factors")
+    )
     
   )
 )
 
-server = function(input, output){
+server = function(input, output) {
+  bins_a = eventReactive(input$go, {
+    input$bins_pref_a
+  })
+  bins_b = eventReactive(input$go, {
+    input$bins_pref_b
+  })
+  bins_indiff = eventReactive(input$go, {
+    input$bins_indiff
+  })
   
-    
-    bins_a = eventReactive(input$go, {
-      input$bins_pref_a
+  
+  observe({
+    input$error_pref
+    output$error_pref_selection = renderUI({
+      x = as.numeric(input$error_pref)
+      withMathJax(
+        sprintf(
+          "The inequality constraints for the deviation from a deterministic choice are
+                           $$\\quad %.03f \\leq P \\leq 1$$",
+          1 - x
+        )
+      )
     })
-    bins_b = eventReactive(input$go, {
-      input$bins_pref_b
+  })
+  
+  
+  observe({
+    input$error_indiff
+    output$error_indiff_selection = renderUI({
+      y = as.numeric(input$error_indiff)
+      withMathJax(
+        sprintf(
+          "The inequality constraints for the deviation from coin flipping are
+                          $$%.03f \\leq P \\leq %.03f$$",
+          0.5 - y,
+          0.5 + y
+        )
+      )
     })
-    bins_indiff = eventReactive(input$go, {
-      setdiff(1:34, c(bins_a(), bins_b()))
-    })
-    tau = eventReactive(input$go, {
-      input$tau
-    })
+  })
+  
+  error_val_pref = eventReactive(input$go, {
+    as.numeric(input$error_pref)
+  })
+  
+  error_val_indiff = eventReactive(input$go, {
+    as.numeric(input$error_indiff)
+  })
+  
+  
+  
+  results = eventReactive(input$go, {
+    req(subjects, bfs_all_sub_00_to_90)
     
+    bfs = sapply(
+      subjects,
+      get_bf_individual,
+      df = bfs_all_sub_00_to_90,
+      bins_indiff = bins_indiff(),
+      bins_pref_a = bins_a(),
+      bins_pref_b = bins_b(),
+      tau_value_indiff = 2 * error_val_indiff(),
+      tau_value_pref = 1 - 2 * error_val_pref()
+    )
     
-    results = eventReactive(input$go, {
-      
-      req(subjects, bfs_all_sub_00_to_33)
-      
-      bfs = sapply(subjects, get_bf_individual,  
-                   df = bfs_all_sub_00_to_33, 
-                   bins_indiff = bins_indiff(), 
-                   bins_pref_a = bins_a(), 
-                   bins_pref_b = bins_b(),
-                   tau_value = tau())
-      
-      bfs = as.data.frame(bfs)
-      colnames(bfs) = 'bf'
-      bfs = bfs %>%
-        mutate(interpretation = case_when(bf < 1/100 ~ "decisive evidence for unconstrained",
-                                          bf >= 1/100 & bf < 1/30 ~ "very strong evidence for unconstrained",
-                                          bf >= 1/30 & bf < 1/10 ~ "strong evidence for unconstrained",
-                                          bf >= 1/10 & bf < 1/3 ~ "substantial evidence for unconstrained",
-                                          bf >= 1/3 & bf < 1 ~ "anecdotal evidence for unconstrained",
-                                          bf >= 1 & bf < 3 ~ "anecdotal evidence for constrained",
-                                          bf >= 3 & bf < 10 ~ "substantial evidence for constrained",
-                                          bf >= 10 & bf < 30 ~ "strong evidence for constrained",
-                                          bf >= 30 & bf < 100 ~ "very strong evidence for constrained",
-                                          bf >= 100 ~ "decisive evidence for constrained"))
-      
-      bfs$interpretation = factor(bfs$interpretation, levels = c("decisive evidence for unconstrained", 
-                                                                 "very strong evidence for unconstrained",
-                                                                 "strong evidence for unconstrained",
-                                                                 "substantial evidence for unconstrained",
-                                                                 "anecdotal evidence for unconstrained",
-                                                                 "anecdotal evidence for constrained",
-                                                                 "substantial evidence for constrained",
-                                                                 "strong evidence for constrained",
-                                                                 "very strong evidence for constrained",
-                                                                 "decisive evidence for constrained"))
-      
-      bfs
-    })
+    bfs = as.data.frame(bfs)
+    colnames(bfs) = 'bf'
+    bfs = bfs %>%
+      mutate(
+        interpretation = case_when(
+          bf < 1 / 100 ~ "decisive evidence for unconstrained",
+          bf >= 1 / 100 &
+            bf < 1 / 30 ~ "very strong evidence for unconstrained",
+          bf >= 1 / 30 &
+            bf < 1 / 10 ~ "strong evidence for unconstrained",
+          bf >= 1 / 10 &
+            bf < 1 / 3 ~ "substantial evidence for unconstrained",
+          bf >= 1 / 3 &
+            bf < 1 ~ "anecdotal evidence for unconstrained",
+          bf >= 1 &
+            bf < 3 ~ "anecdotal evidence for constrained",
+          bf >= 3 &
+            bf < 10 ~ "substantial evidence for constrained",
+          bf >= 10 &
+            bf < 30 ~ "strong evidence for constrained",
+          bf >= 30 &
+            bf < 100 ~ "very strong evidence for constrained",
+          bf >= 100 ~ "decisive evidence for constrained"
+        )
+      )
     
+    bfs$interpretation = factor(
+      bfs$interpretation,
+      levels = c(
+        "decisive evidence for unconstrained",
+        "very strong evidence for unconstrained",
+        "strong evidence for unconstrained",
+        "substantial evidence for unconstrained",
+        "anecdotal evidence for unconstrained",
+        "anecdotal evidence for constrained",
+        "substantial evidence for constrained",
+        "strong evidence for constrained",
+        "very strong evidence for constrained",
+        "decisive evidence for constrained"
+      )
+    )
     
-    output$bf_dist = renderPlotly({
-      
+    bfs
+  })
+  
+  
+  output$bf_dist = renderPlotly({
     p = results() %>% count(interpretation) %>%
-        ggplot(aes(x = interpretation, y = n)) + 
-        geom_col(fill = "navy") + 
-        coord_flip() +
-        labs(x = "Bayes Factor Interpretation", y = "Count")
+      ggplot(aes(x = interpretation, y = n)) +
+      geom_col(fill = "navy") +
+      coord_flip() +
+      labs(x = "Bayes Factor Interpretation", y = "Count")
     
-      ggplotly(p, tooltip = "y")
-      
-      })
+    ggplotly(p, tooltip = "y")
     
-    
-    output$group_bf = renderText(
-
-      paste("The Group Bayes Factor across all subjects is", exp(sum(log(results()$bf))))
-
-    )
-    
-    output$exportBFs = downloadHandler(
-      filename = function() {
-             paste('bayes_factors', Sys.Date(), '.csv', sep='')
-           },
-      content = function(con) {
-             write.csv(results()$bf, con)
-           }
-    )
-    
+  })
+  
+  
+  output$group_bf = renderText(paste("The Group Bayes Factor across all subjects is", exp(sum(log(
+    results()$bf
+  )))))
+  
+  output$exportBFs = downloadHandler(
+    filename = function() {
+      paste('bayes_factors', Sys.Date(), '.csv', sep = '')
+    },
+    content = function(con) {
+      write.csv(results()$bf, con)
+    }
+  )
   
 }
 
--- get_bf_individual.R
+++ get_bf_individual.R
@@ -1,30 +1,31 @@
-# BF for specified bins, a given tau value, and a given subject
-get_bf_individual = function(df, bins_indiff = NULL, bins_pref_a = NULL, bins_pref_b = NULL, tau_value, subject){
+# BF for specified bins, a given tau value, and given subject(s)
+get_bf_individual = function(df, bins_indiff = NULL, bins_pref_a = NULL, bins_pref_b = NULL, tau_value_indiff, tau_value_pref, subjects){
   
   if (length(bins_indiff) > 0) {
-    bfs_indiff = df %>% filter(tau == tau_value & Subject %in% subject) %>%
-    filter(Bin %in% paste0("bin", bins_indiff)) %>%
-    select(bf_indiff) %>% pull(-1)
+    bfs_indiff = df %>% filter(Bin %in% paste0("bin", bins_indiff)) %>%
+      filter(Subject %in% subjects) %>%
+      filter(tau == tau_value_indiff) %>%
+      select(indiff) %>% pull(-1)
   } else {
     bfs_indiff = NA
   }
   
   
   if (length(bins_pref_a) > 0){
-    bfs_pref_a = df %>%
-    filter(tau == tau_value & Subject %in% subject) %>%
-    filter(Bin %in% paste0("bin", bins_pref_a)) %>%
-    select(bf_pref_a) %>% pull(-1)
+    bfs_pref_a = df %>% filter(Bin %in% paste0("bin", bins_pref_a)) %>%
+      filter(Subject %in% subjects) %>%
+      filter(tau == tau_value_pref) %>%
+      select(a) %>% pull(-1)
   } else {
     bfs_pref_a = NA
   }
   
   
   if (length(bins_pref_b) > 0){
-    bfs_pref_b = df %>%
-    filter(tau == tau_value & Subject %in% subject) %>%
-    filter(Bin %in% paste0("bin", bins_pref_b)) %>%
-    select(bf_pref_b) %>% pull(-1)
+    bfs_pref_b = df %>% filter(Bin %in% paste0("bin", bins_pref_b)) %>%
+      filter(Subject %in% subjects) %>%
+      filter(tau == tau_value_pref) %>%
+      select(b) %>% pull(-1)
   } else {
     bfs_pref_b = NA
   }
